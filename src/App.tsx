2203c44951d02cfed978032918b9e2db252eccf2
MrMatreshka <kirill17031994>
2020-11-20 10:53:18 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SignIn } from './pages/Signin';
+// import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/*<Switch>*/}
+        {/*    <Route path="/signin" component={SignIn} exact />*/}
+        {/*        /!*<Route path="/home" component={Home} />*!/*/}
+        {/*        /!*<Route path="/user" component={UserPage} />*!/*/}
+
+        {/*</Switch>*/}
+        <SignIn/>
     </div>
   );
 }
